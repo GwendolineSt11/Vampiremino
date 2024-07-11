edf67a40e91865aba8dbe75b41c48e0531b999a9
Khristine <kikikhristine@hotmail.com>
2024-07-11 16:48:58 -0600

    Auto-upgrade

--- Vampiremino/urls.py
+++ Vampiremino/urls.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', lambda request: HttpResponse("Test URL works!")),
-    path('interactions/', bot_view, name='bot_view'),
+    path('aminointer/', bot_view, name='bot_view'),
     path('', lambda request: HttpResponse("This is an angel of electricity blood getting back to you." "\n Hey, welcome!" " \nCookies?")),
     ]
 
